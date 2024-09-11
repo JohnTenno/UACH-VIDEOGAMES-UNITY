30232dce1ce4dfb6a014e610b5fd571ef5be591e
JohnTenno <johntenno117@hotmail.com>
2024-09-10 21:26:12 -0600

    updates

---
+++ Assets/TurtlesSpawnPoint.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleSpawnPoint : MonoBehaviour
+{
+    [SerializeField] protected bool isRightMovement = true;
+    [SerializeField] protected Transform container;
+
+    [SerializeField] protected GameObject turtlePrefab;
+
+
+    public void Spawn()
+    {
+        Vector3 position = new Vector3(
+            transform.position.x,
+            GetRandomHeightPosition(transform.position.y),
+            transform.position.z
+        );
+
+        GameObject turtle = Instantiate(
+            turtlePrefab,
+            position,
+            turtlePrefab.transform.rotation,
+            container.transform
+        );
+
+        TurtleMovementFix turtleMovement = turtle.GetComponent<TurtleMovementFix>();
+        turtleMovement.isRightMovement = isRightMovement;
+    }
+
+    float GetRandomHeightPosition(float origin)
+    {
+        float cameraHeight = Camera.main.orthographicSize;
+        float min = origin;
+        float max = origin + cameraHeight + 2;
+
+        float randomValue = Random.Range(min, max);
+        return randomValue;
+    }
+}
---
+++ Assets/scripts/TurtleDispawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleDispawn : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("turtle"))
+        {
+            Debug.Log("tortuga");
+            Destroy(other.gameObject);
+        }
+    }
+}
